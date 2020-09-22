2d42a492149e52eb27f2a7557bc1b025fd47d967
Wynand Tredoux <wynand.tredoux@ucalgary.ca>
2020-09-21 18:46:09 -0600

    Added stats calcs

--- main.py
+++ main.py
@@ -20,23 +20,24 @@ def main(CNTfile, MesFile, CNTheader=1, Mesheader=1, suppress_print=False):
     # build weight matrix P
     P = buildP(MES, sigma0)
 
-    # Main loop start ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Main loop START ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # some settings for the loop. These could be put in a separate settings file
     deltasum = 100  # initialize deltasum to some large value
     threshold = 0.0000000001  # threshold for minimum deltasum
+    # this threshold is way smaller than it needs to be, but since the function converges very quickly this doesn't noticeably impact performance
     maxit = 100  # maximum iterations before breaking the loop
-    count = 1 # initialize iteration counter
-    # loop until the sum of delta is less than some threshold
+    count = 1  # initialize iteration counter
+    # loop until the absolute sum of the elements in delta is less than some small threshold
     while deltasum > threshold:
         if not suppress_print:
             print("Iteration: " + str(count))  # print iteration number for the user
-        # also break if maximum iterations are reached
+        # also break if maximum number of iterations is reached
         if count >= maxit:
             print("Maximum number of iterations reached")
             print("Break")
             break
 
-        # build the design matrix A and the misclosure matrix w
+        # build the design matrix A and the misclosure vector w
         A, w = buildAw(CNT, MES, x)
 
         # calculate solution
@@ -46,7 +47,6 @@ def main(CNTfile, MesFile, CNTheader=1, Mesheader=1, suppress_print=False):
         Cx = np.linalg.inv(N)
         delta = -Cx @ u
 
-        #x = x + delta
         deltasum = 0
         # for each element of delta
         for i in range(0, len(delta)):
@@ -54,10 +54,39 @@ def main(CNTfile, MesFile, CNTheader=1, Mesheader=1, suppress_print=False):
             deltasum = deltasum + abs(delta[i])  # sum absolute value to deltasum
 
         if not suppress_print:
-            print("deltasum = " + str(deltasum[0]) + "\n\n")
+            print("deltasum = " + str(deltasum[0]) + "\n")
         count = count + 1  # iterate count
+    # Main loop END ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Calculate residuals
+    rhat = A @ delta + w
+    # Adjusted observations
+    lhat = np.zeros([len(MES), 1])
+    for i in range(0, len(MES)):
+        lhat[i] = MES[i][3] + rhat[i]
+    # Calculate posteriori variance factor and unit variance factor
+    sigma0hat = (rhat.transpose() @ P @ rhat)/(len(MES) - len(x))
+    sigma0hat = sigma0hat[0][0]  # convert to float from ndarray
+    unitvar = sigma0hat/sigma0
+    # Calculate the v-c matrix of unknowns Cxhat
+    Cxhat = sigma0hat * Cx
+    # Calculate the v-c matrix of measurements Clhat
+    Clhat = A @ Cxhat @ At
+    # Calculating the v-c matrix of residuals Crhat
+    Crhat = sigma0hat * np.linalg.inv(P) - Clhat
+    # Calculate error ellipse
+    # for each 2x2 sub-matrix in Cxhat
+    for i in range(0, len(Cxhat), 2):
+        # extract sub matrix
+        subM = np.zeros([2, 2])
+        subM[0][0] = Cxhat[i][i]
+        subM[0][1] = Cxhat[i][i+1]
+        subM[1][1] = Cxhat[i+1][i+1]
+        subM[1][0] = Cxhat[i+1][i]
 
     if not suppress_print:
+        print('posteriori variance factor: ' + str(sigma0hat)
+              + '\nunit variance factor: ' + str(unitvar)
+              )
         print('done!')
 
 
