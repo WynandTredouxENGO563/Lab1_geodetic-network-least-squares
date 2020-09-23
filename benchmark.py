286d26cc83ad9b1ac9ef9c73eeb627e6bc136c3e
Wynand Tredoux <wynand.tredoux@ucalgary.ca>
2020-09-23 12:52:21 -0600

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -2,6 +2,11 @@ import timeit
 import functools
 from main import *
 
-n = 10000
-total = timeit.timeit(functools.partial(main, "coordinates.cnt", "measurements.mes", suppress_print=True, plot=False), setup='gc.enable()', number=n)
-print("Average Time: " + str(total/n) + " seconds")
+
+def benchmark(n=10000):
+    total = timeit.timeit(functools.partial(main, "coordinates.cnt", "measurements.mes", suppress_print=True, plot=False), setup='gc.enable()', number=n)
+    print("Total Time: " + str(total) + " seconds\nAverage Time: " + str(total/n) + " seconds")
+
+
+if __name__ == '__main__':
+    benchmark()
