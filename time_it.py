3db3c2fb4920babb892a07c0d8878a41d361bcec
Wynand Tredoux <wynand.tredoux@ucalgary.ca>
2020-09-23 00:07:09 -0600

    added option to suppress plotting

--- time_it.py
+++ time_it.py
@@ -3,5 +3,5 @@ import functools
 from main import *
 
 n = 10000
-total = timeit.timeit(functools.partial(main, "coordinates.cnt", "measurements.mes", suppress_print=True), setup='gc.enable()', number=n)
+total = timeit.timeit(functools.partial(main, "coordinates.cnt", "measurements.mes", suppress_print=True, plot=False), setup='gc.enable()', number=n)
 print("Average Time: " + str(total/n) + " seconds")
